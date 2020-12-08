16b3a2b653136cfd34e5202f0352ea7d4955358a
Nicolas Hauser <nicolas.hauser@edu.ece.fr>
2020-12-08 23:45:03 +0100

    from scratch

--- userapi/src/models/create.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// connection à la base de donnée dbClient du local host 27017
-mongoose.connect('mongodb://localhost:27017/dbClient',{useNewUrlParser: true},(err)=>{
-    if(!err){console.log('MongoDB is connected')}
-    else{console.log('Error of connection'+ err)}
-});
-
-require('./client.model');
