9ac41392d0a600380a05bbde07bff13a1b1a99f5
Nicolas Hauser <nicolas.hauser@edu.ece.fr>
2020-12-21 22:22:01 +0100

    docker-compose working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DevOpsProjet_Hauser_Manet_Puy
 DevOps Project Github
+
+## Docker Compose Usage
+
+ - Place yourself in the root directory
+ - run the command "docker-compose build"
+ - run the command "docker-compose up"
+ - web app is waiting for you on "http://localhost:3000"
--- userapi/src/models/create.js
+++ userapi/src/models/create.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connection à la base de donnée dbClient du local host 27017
-mongoose.connect('mongodb://localhost:27017/dbClient',{useNewUrlParser: true},(err)=>{
+mongoose.connect('mongodb://mongo:27017/dbClient',{useNewUrlParser: true},(err)=>{
     if(!err){console.log('MongoDB is connected')}
     else{console.log('Error of connection'+ err)}
 });
